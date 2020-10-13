485fad401ba8396922e29e5c5e10b322d9e4b158
Zhang Daiying <zhangdaiying@xiaomi.com>
2020-10-13 13:59:38 +0800

    change exec() to execFile()

--- test/open.js
+++ test/open.js
@@ -3,7 +3,7 @@ var op = require('../lib/index');
 'chrome safari firefox opera'.split(' ').forEach(function (browser) {
   op.detect(browser).then(function (path) {
     console.log(browser, '==>', path);
-    op.open(browser, 'https://www.google.com', 'http://127.0.0.1:5525');
+    op.open(browser, 'https://www.google.com/', 'http://127.0.0.1:5525');
   }).catch(function (err) {
     console.error(browser, 'detect fail:', err);
   });
